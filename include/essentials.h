3032adaaea882f459874f71a9c1ceefef2998460
MJ Akbari <mojav.akbari@gmail.com>
2024-10-08 01:06:13 +0330

    cmake and finder

--- include/essentials.h
+++ include/essentials.h
@@ -15,20 +15,20 @@
 #define FPS 60 // 60 fps is default. Don't set higher than 120.
 #define FRAME_TIME (1000/FPS)
 
-#define GAME_BGP "./src/resources/backGround.bmp"
-#define FONT_P "./src/resources/LaFamilia.ttf"
-#define LIST_P "./src/resources/scoresList.bin"
+#define GAME_BGP "../resources/backGround.bmp"
+#define FONT_P "../resources/LaFamilia.ttf"
+#define LIST_P "../resources/scoresList.bin"
 
-#define JET_PP "./src/resources/Jet.bmp"
-#define MINE_PP "./src/resources/mine.bmp"
-#define DRONE_PP "./src/resources/drone.bmp"
-#define BOMB_PP "./src/resources/bomb.bmp"
-#define BOMBER_PP "./src/resources/bomber.bmp"
-#define FIGHTER_PP "./src/resources/fighter.bmp"
-#define SHIP_PP "./src/resources/ship.bmp"
-#define HEALTH_PP "./src/resources/health.bmp"
-#define BULLET_UP_PP "./src/resources/bulletUp.bmp"
-#define EXPLODE_PP "./src/resources/explode.bmp"
+#define JET_PP "../resources/Jet.bmp"
+#define MINE_PP "../resources/mine.bmp"
+#define DRONE_PP "../resources/drone.bmp"
+#define BOMB_PP "../resources/bomb.bmp"
+#define BOMBER_PP "../resources/bomber.bmp"
+#define FIGHTER_PP "../resources/fighter.bmp"
+#define SHIP_PP "../resources/ship.bmp"
+#define HEALTH_PP "../resources/health.bmp"
+#define BULLET_UP_PP "../resources/bulletUp.bmp"
+#define EXPLODE_PP "../resources/explode.bmp"
 
 // General Essentials
 extern SDL_Window *window;
